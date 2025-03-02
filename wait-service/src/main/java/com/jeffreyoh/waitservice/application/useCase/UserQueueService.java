73df7bcc991e4654c1732d0799ae9a510574c608
오태현 <jeffreyoh121@gmail.com>
2025-03-02 22:23:49 +0900

    대기열을 처리하기 위한 스케줄러 등록

--- wait-service/src/main/java/com/jeffreyoh/waitservice/application/useCase/UserQueueService.java
+++ wait-service/src/main/java/com/jeffreyoh/waitservice/application/useCase/UserQueueService.java
@@ -3,12 +3,18 @@ package com.jeffreyoh.waitservice.application.useCase;
 import com.jeffreyoh.waitservice.application.port.in.userQueue.UserQueueUseCase;
 import com.jeffreyoh.waitservice.infrastructure.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService implements UserQueueUseCase {
@@ -16,7 +22,11 @@ public class UserQueueService implements UserQueueUseCase {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled:false}")
+    private Boolean scheduling;
 
     @Override
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -54,6 +64,24 @@ public class UserQueueService implements UserQueueUseCase {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
             .defaultIfEmpty(-1L)
             .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 3000)
+    public void scheduleAllowUser() {
+        if (!scheduling) return;
+
+        final long maxAllowUserCount= 3L;
+
+        reactiveRedisTemplate.scan(
+            ScanOptions.scanOptions()
+                .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                .count(100)
+                .build()
+            )
+            .map(key -> key.split(":")[2])
+            .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+            .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+            .subscribe();
     }
 
 }
